7f74624d432b31fd59ebd77e5bc244fe0bc32a12
ProgrammerRez <Programmerrez2318@gmail.com>
2025-08-30 16:33:13 +0500

    Finalized the Project

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-    X, y, total_words, max_len, tokenizer = preprocess_data(
-        filepath=dataset_path, num_words=2000, padding="post"
-    )
-    model = build_model(total_words=total_words, max_len=max_len, mode="SimpleRNN")
-
-    model, history, loss, acc = train_or_retrain(
-        X=X, y=y, model=model, earlystop=False, show_summary=True
-    )
